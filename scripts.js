2668705d308b3fe32fd6937cae8abfc5fb3525e9
Alexey Volkov <vollkovalex1991@mail.ru>
2023-03-28 19:51:05 +0400

    2nd commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const iconMenu = document.querySelector('.burger-menu')
+if(iconMenu){
+    const menuBody = document.querySelector('.menu-body')
+    iconMenu.addEventListener('click', function(e){
+        iconMenu.classList.toggle('_active')
+        menuBody.classList.toggle('_active')
+    })
+}
