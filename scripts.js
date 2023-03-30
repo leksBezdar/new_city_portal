804dff62be718f9b93ebbdc88de2f8b5bd93ff9a
Alexey Volkov <vollkovalex1991@mail.ru>
2023-03-30 20:43:26 +0400

    Nikita geniy

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const iconMenu = document.querySelector('.burger-menu')
-if(iconMenu){
+if (iconMenu) {
     const menuBody = document.querySelector('.menu-body')
-    iconMenu.addEventListener('click', function(e){
+    iconMenu.addEventListener('click', function (e) {
         iconMenu.classList.toggle('_active')
         menuBody.classList.toggle('_active')
     })
