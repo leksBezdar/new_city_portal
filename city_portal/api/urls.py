6b70fee5fdc7cada55e0089930db6b6ab71489df
Wizard <wizard.or.volshebnik@gmail.com>
2023-03-31 21:30:14 +0400

    changes for uncle Lesha

--- city_portal/api/urls.py
+++ city_portal/api/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 
 urlpatterns = [
-    # path('', WomenHome.as_view(), name='home'),
+    path('', base),
 ]
--- city_portal/api/views.py
+++ city_portal/api/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
 
+
+def base(request):
+    return render(request, 'api/base.html')
+
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
