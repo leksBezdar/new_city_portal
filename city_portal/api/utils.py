2eacfb2d6ee629a4a18d1c0b6add8087d4d52025
Wizard <wizard.or.volshebnik@gmail.com>
2023-04-01 10:26:52 +0400

    Add utils

---
+++ city_portal/api/utils.py
@@ -0,0 +1,6 @@
+menu = [{'title': "Главная", 'url_name': 'home'},
+        {'title': "Заявки", 'url_name': 'application'},
+        {'title': "Команда", 'url_name': 'team'},
+        {'title': "Контакты", 'url_name': 'contact'},
+        ]
+
