17df6c7396343b9438b9da680370238057a9bf97
Wizard <wizard.or.volshebnik@gmail.com>
2023-04-02 19:18:03 +0400

    Configuring the work of posts via the API

--- city_portal/api/models.py
+++ city_portal/api/models.py
@@ -3,26 +3,26 @@ from django.urls import reverse
 
 
 class Api(models.Model):
-    problem = models.CharField(max_length=50, verbose_name="Проблема")
+    problem = models.CharField(max_length=50, verbose_name="Проблема", blank=True)
     description = models.TextField(blank=True, verbose_name="Описание")
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото", blank=True)
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категории")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категории", blank=True)
 
     def __str__(self):
         return self.problem
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория", blank=True)
 
     def __str__(self):
         return self.name
 
 class Menu(models.Model):
-    name = models.CharField('Название', max_length=100)
-    url = models.CharField('Ссылка', max_length=255, unique=True)
+    name = models.CharField('Название', max_length=100, blank=True)
+    url = models.CharField('Ссылка', max_length=255, unique=True, blank=True)
 
     def __str__(self):
         return str(self.name)
--- city_portal/api/templatetags/common_tags.py
+++ city_portal/api/templatetags/common_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from api.models import Menu
+from api.models import *
 register = template.Library()
 
 
@@ -8,4 +8,12 @@ def show_top_menu(context):
     menu_items = Menu.objects.all()
     return {
         "menu_items": menu_items,
+    }
+
+
+@register.inclusion_tag('api/post.html', takes_context=True)
+def show_post(context):
+    api_items = Api.objects.latest('time_update')
+    return {
+        "api_items": api_items,
     }
