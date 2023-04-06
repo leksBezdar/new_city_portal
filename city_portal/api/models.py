ff5af1d3bb7aecfeeb6675af01b87267b977bbae
Wizard <wizard.or.volshebnik@gmail.com>
2023-04-06 16:23:06 +0400

    Optimization

--- city_portal/api/models.py
+++ city_portal/api/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Post(models.Model):
-    problem = models.CharField(max_length=50, verbose_name="Название проблемы", blank=True)
+    title = models.CharField(max_length=50, verbose_name="Название проблемы", blank=True)
     content = models.TextField(blank=True, verbose_name="Текст статьи")
-    img_before = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото до", blank=True)
-    img_after = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото после", null=True)
+    img_before = models.ImageField(upload_to="images/", verbose_name="Фото до", blank=True)
+    img_after = models.ImageField(upload_to="images/", verbose_name="Фото после", null=True)
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категории", blank=True)
 
     def __str__(self):
-        return self.problem
+        return self.title
 
 
 class Category(models.Model):
--- city_portal/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import *
-
-
-class ApiSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
