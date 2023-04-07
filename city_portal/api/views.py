83dacc168cc77c53000db6c51234fbdb3d6e6fc3
Wizard <wizard.or.volshebnik@gmail.com>
2023-04-07 20:43:58 +0400

    Fix code commit

--- city_portal/api/views.py
+++ city_portal/api/views.py
@@ -1,31 +1,11 @@
 from django.http import HttpResponseNotFound
-from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
+from django.shortcuts import render
 
 from .models import *
-from .forms import *
 
 
-def registration(request):
-    error = ''
-    if request.method == "POST":
-        form = CreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('')
-        else:
-            error = "Одно или несколько полей были заполнены неверно"
-
-    form = CreationForm
-    log_form = LoginForm
-    data = {
-        'form': form,
-        'log_form': log_form,
-        'error': error
-    }
-    return render(request, 'api/signup.html', data)
-
+def base(request):
+    return render(request, 'api/base.html')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
