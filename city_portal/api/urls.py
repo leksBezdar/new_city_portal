25c4c226bf89b34d3a3f1cae837962b519f160ce
Wizard <wizard.or.volshebnik@gmail.com>
2023-04-08 14:04:31 +0400

    Working registration

--- city_portal/api/urls.py
+++ city_portal/api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', base),
-    path('', RegisterUser.as_view()),
+    path('registr/', regist),
 ]
--- city_portal/api/views.py
+++ city_portal/api/views.py
@@ -1,32 +1,22 @@
-from django.contrib.auth.views import LoginView
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-
-from .forms import *
-from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 
-def base(request):
-    return render(request, 'api/register_and_login.html')
-
-
-# class LoginUser(LoginView):
-#     form_class = LoginUserForm
-#     template_name = 'api/register_and_login.html'
-
-
-class RegisterUser(CreateView):
-    form_class = RegisterUserForm
-    template_name = 'api/register_and_login.html'
-    success_url = reverse_lazy('home')
-
-
-# def login_or_reg(request, *args, **kwargs):
-#     if 'password2' in request.POST:
-#         return RegisterUser.as_view()(request)  # Не уверен, нужно ли передавать args и kwargs
-#     return LoginUser.as_view()(request)
+def regist(request):
+    data = {}
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            data['form'] = form
+            data['res'] = "Всё прошло успешно"
+        return render(request, 'api/registr.html', data)
+    else:
+        form = RegisterUserForm()
+        data['form'] = form
+    return render(request, 'api/registr.html', data)
 
 
 def pageNotFound(request, exception):
