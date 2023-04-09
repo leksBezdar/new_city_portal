9709188f4ae96d94e315590b457b37b530fb85f0
Wizard <wizard.or.volshebnik@gmail.com>
2023-04-10 00:04:20 +0400

    Minor edits to the sign-up and login logic. Start implementing profile logic and creating a ticket

--- city_portal/api/urls.py
+++ city_portal/api/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', regist, name="regist"),
-    path('home/', home, name='home'),
-    path('login/', login, name="login"),
+    path('', RegisterUser.as_view(), name="regist"),
+    path('profile/', profile, name='profile'),
+    path('login/', LoginUser.as_view(), name="login"),
     path('logout/', logout_user, name='logout'),
+    path('application/', application, name="application"),
 ]
--- city_portal/api/views.py
+++ city_portal/api/views.py
@@ -1,49 +1,38 @@
 from django.contrib.auth import authenticate, logout, login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
-from .forms import RegisterUserForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import RegisterUserForm, LoginUserForm
 
 
-def home(request):
-    return render(request, "api/base.html")
+def profile(request):
+    return render(request, "api/profile.html")
+
+def application(request):
+    return render(request, "api/application.html")
 
 
-def regist(request):
-    data = {}
-    if request.method == 'POST':
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            data['form'] = form
-            data['res'] = "Всё прошло успешно"
-            return redirect("login")
-        return render(request, 'api/registr.html', data)
-    else:
-        form = RegisterUserForm()
-        data['form'] = form
-    return render(request, 'api/registr.html', data)
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'api/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('profile')
 
 
-def login(request):
-    context = {}
-    if request.method == "POST":
-        form = AuthenticationForm(request.POST)
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'api/registr.html'
+    success_url = reverse_lazy('login')
 
-        if form.is_valid:  # returns True
-            name = request.POST["username"]
-            pswd = request.POST["password"]
-            user = authenticate(request, username=name, password=pswd)
-            print("user = " + str(user))  # always returns None
-
-            if user:
-                print("valid user " + str(user))
-                auth_login(request, user)
-                print("user is " + str(request.user.is_authenticated))
-                return redirect("home")
-    form = AuthenticationForm()
-    context['login_form'] = form
-    return render(request, 'api/login.html', context)
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect('login')
 
 
 def logout_user(request):
